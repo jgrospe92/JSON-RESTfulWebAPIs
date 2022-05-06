bc3c3becdc9d1df6ca62ca187dfba7d6b83a830b
jgrospe92 <jeffreygrospe@yahoo.com>
2022-05-06 18:44:47 -0400

    Added app folder

---
+++ Assignement04/Assignement04/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace Assignement04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.IdTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.previousButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "JSON Employee Record";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Employee ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 19);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Employee Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 19);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Employee Salary:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 19);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Employee Age:";
+            // 
+            // IdTextBox
+            // 
+            this.IdTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IdTextBox.Location = new System.Drawing.Point(142, 60);
+            this.IdTextBox.Name = "IdTextBox";
+            this.IdTextBox.Size = new System.Drawing.Size(240, 24);
+            this.IdTextBox.TabIndex = 2;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameTextBox.Location = new System.Drawing.Point(142, 93);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(240, 24);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salaryTextBox.Location = new System.Drawing.Point(142, 126);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.salaryTextBox.Size = new System.Drawing.Size(240, 24);
+            this.salaryTextBox.TabIndex = 2;
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ageTextBox.Location = new System.Drawing.Point(142, 159);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ageTextBox.Size = new System.Drawing.Size(240, 24);
+            this.ageTextBox.TabIndex = 2;
+            // 
+            // previousButton
+            // 
+            this.previousButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.previousButton.Location = new System.Drawing.Point(16, 214);
+            this.previousButton.Name = "previousButton";
+            this.previousButton.Size = new System.Drawing.Size(98, 48);
+            this.previousButton.TabIndex = 3;
+            this.previousButton.Text = "Previous Record";
+            this.previousButton.UseVisualStyleBackColor = true;
+            this.previousButton.Click += new System.EventHandler(this.previousButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(150, 214);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(98, 48);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextButton.Location = new System.Drawing.Point(284, 214);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(98, 48);
+            this.nextButton.TabIndex = 3;
+            this.nextButton.Text = "Next Record";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 310);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.previousButton);
+            this.Controls.Add(this.ageTextBox);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.IdTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "JSON RESTful API Employee Record Viewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox IdTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.TextBox ageTextBox;
+        private System.Windows.Forms.Button previousButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button nextButton;
+    }
+}
+
---
+++ Assignement04/Assignement04/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using RestSharp;
+using System.Net;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading;
+
+
+namespace Assignement04
+{
+    public partial class Form1 : Form
+    {
+
+        private static Datum[] employees;
+        private static int counter = 0;
+        private static int empTotal;
+
+        public Form1()
+        {
+            InitializeComponent();
+            GetEmployeeData();
+        }
+
+        private static void GetEmployeeData()
+        {
+    
+            try
+            {
+                var client = new RestClient("http://dummy.restapiexample.com/api/v1/");
+                var request = new RestRequest("employees");
+                var response = client.Execute(request);
+
+                if(response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    string rawResponse = response.Content;
+                    Rootobject result = JsonConvert.DeserializeObject<Rootobject>(rawResponse);
+                    employees = result.data;
+                    empTotal = employees.Length;
+
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Fail to retireve JSON data", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+        }
+
+        private void InitializeInputBoxes(Datum datum)
+        {
+            
+            IdTextBox.Text = (datum.id).ToString();
+            nameTextBox.Text = (datum.employee_name).ToString();
+            salaryTextBox.Text = (datum.employee_salary).ToString("C");
+            ageTextBox.Text = (datum.employee_age).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Datum currentEmp = employees[0];
+            InitializeInputBoxes(currentEmp);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            if (counter >= empTotal - 1)
+            {
+                counter = -1;
+            }
+            try
+            {
+                Datum currentEmp = employees[++counter];
+                InitializeInputBoxes(currentEmp);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid Record.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void previousButton_Click(object sender, EventArgs e)
+        {
+            if (counter <= 0)
+            {
+                counter = empTotal;
+            }
+            try
+            {
+                Datum currentEmp = employees[--counter];
+                InitializeInputBoxes(currentEmp);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid Record.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+
+
+    // Classes provided by Postman.
+    public class Rootobject
+    {
+        public string status { get; set; }
+        public Datum[] data { get; set; }
+        public string message { get; set; }
+    }
+
+    public class Datum
+    {
+        public int id { get; set; }
+        public string employee_name { get; set; }
+        public int employee_salary { get; set; }
+        public int employee_age { get; set; }
+
+    }
+
+}
